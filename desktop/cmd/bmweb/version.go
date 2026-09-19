7e0a1e7f5ffccf9dde5dbb1c7732b7bc7c3b23f7
SkyGazer <qinlong9518@gmail.com>
2026-09-19 23:16:44 +0800

    v1.16.0: rename 彬煤助手 + titlebar version + login saved-accounts + fix Last

--- desktop/cmd/bmweb/version.go
+++ desktop/cmd/bmweb/version.go
@@ -4,8 +4,8 @@ package main
 
 // 应用版本信息
 const (
-	AppVersion     = "1.15.1"
-	AppVersionCode = 19
+	AppVersion     = "1.16.0"
+	AppVersionCode = 20
 	OfficialSite   = "https://qinlong9518.github.io/bmzs/"
 )
 
