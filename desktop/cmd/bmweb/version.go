d63c3a8c11a295037d0a955d08ec95fd265deb6e
SkyGazer <qinlong9518@gmail.com>
2026-09-19 21:31:56 +0800

    fix: restore sig() (blank points) + upd dialog cancel btn + v1.15.1

--- desktop/cmd/bmweb/version.go
+++ desktop/cmd/bmweb/version.go
@@ -4,8 +4,8 @@ package main
 
 // 应用版本信息
 const (
-	AppVersion     = "1.15.0"
-	AppVersionCode = 18
+	AppVersion     = "1.15.1"
+	AppVersionCode = 19
 	OfficialSite   = "https://qinlong9518.github.io/bmzs/"
 )
 
