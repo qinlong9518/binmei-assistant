21e701462088ff9a4c43f5e7d16a5fe3c683dca8
SkyGazer <qinlong9518@gmail.com>
2026-09-19 12:31:02 +0800

    wails: desktop/cmd/bmweb/version.go

---
+++ desktop/cmd/bmweb/version.go
@@ -0,0 +1,24 @@
+//go:build windows
+
+package main
+
+// 应用版本信息
+const (
+	AppVersion     = "1.13.0"
+	AppVersionCode = 9
+	OfficialSite   = "https://qinlong9518.github.io/binmei-assistant/"
+)
+
+// 在线更新元数据源（镜像优先）
+var metaSources = []string{
+	"https://ghfast.top/https://raw.githubusercontent.com/qinlong9518/binmei-assistant/main/desktop_update.json",
+	"https://ghproxy.net/https://raw.githubusercontent.com/qinlong9518/binmei-assistant/main/desktop_update.json",
+	"https://raw.githubusercontent.com/qinlong9518/binmei-assistant/main/desktop_update.json",
+}
+
+// downloadMirrors GitHub 直链镜像前缀
+var downloadMirrors = []string{
+	"https://ghfast.top/%s",
+	"https://ghproxy.net/%s",
+	"%s",
+}
