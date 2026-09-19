9a301a1819d4281693bddc79f72a62a2c8681f19
SkyGazer <qinlong9518@gmail.com>
2026-09-19 17:35:06 +0800

    feat: auto webview2 install + v1.13.2

--- desktop/cmd/bmweb/version.go
+++ desktop/cmd/bmweb/version.go
@@ -4,8 +4,8 @@ package main
 
 // 应用版本信息
 const (
-	AppVersion     = "1.13.1"
-	AppVersionCode = 10
+	AppVersion     = "1.13.2"
+	AppVersionCode = 11
 	OfficialSite   = "https://qinlong9518.github.io/binmei-assistant/"
 )
 
